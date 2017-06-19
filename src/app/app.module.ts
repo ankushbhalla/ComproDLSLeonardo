a47ae0eae4f6c1f71ee0243ec7c30f6812d15b88
ashish.khandelwal <ashish.khandelwal@comprotechnologies.com>
2017-06-19 18:34:36 +0530

    Commtting the json data updates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'
 
 import { LeonardoModule } from "./leonardo/leonardo.module";
 import { AppComponent } from './app.component';
+
+
+const appRoutes: Routes = [
+  { 
+    path: '',
+    redirectTo: 'v1',
+    pathMatch: 'full' 
+  },
+  {
+    path: 'v1',
+    loadChildren: './leonardo/leonardo.module#LeonardoModule'
+  }
+];
+
 
 @NgModule({
   declarations: [
@@ -14,7 +29,8 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    LeonardoModule
+    LeonardoModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/leonardo/leonardo.module.ts
+++ src/app/leonardo/leonardo.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import { LeonardoComponent } from './leonardo.component';
 import { NavigatorComponent } from './navigator/navigator.component';
@@ -8,10 +9,18 @@ import { WorkspaceComponent } from './workspace/workspace.component';
 
 import { SplitPaneModule } from './verticalSplitter/ng2-split-pane';
 
+const leoRoutes: Routes = [
+  {
+    path: '',
+    component: LeonardoComponent
+  }
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    SplitPaneModule
+    SplitPaneModule,
+    RouterModule.forChild(leoRoutes)
   ],
   exports: [
     LeonardoComponent
