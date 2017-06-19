939e9b7ad35dd1e49fc75bee899685c9cd42bccf
gautambishwas <gautambishwas@comprotechnologies.com>
2017-06-19 14:24:05 +0530

    Adding Ribbon Into Learnado App

--- src/libs/ribbonAll.js
+++ src/libs/ribbonAll.js
@@ -24119,12 +24119,32 @@ ItemGenerators2016.IconTextDesc = ItemGenerators.IconTextDesc.extend({
 		else{
 			text = $itemXml.attr('text');
 		}
-        text = text != null && !text.isEmpty() ? text : "";
+        text = text != null && !Polyfills.string.isEmpty(text) ? text : "";
         return $('<div class="dropdown-item-value" unselectable="on"><b>' + text + '</b></div>');
     }
 });
                   
   
+namespace("ItemGenerators");
+
+ItemGenerators.Separator = ItemGenerators.ItemBase.extend({
+    getItem: function (xItem, type) {
+        var $itemXml = $(xItem);
+        var separatormode = $itemXml.attr('mode');
+        var cssclass;
+        if (separatormode == 'solid') {
+            cssclass = ' dropdown-item-text-separator';
+        }
+        else if (separatormode == 'dashed') {
+            cssclass = ' dropdown-item-text-separator-dashed';
+        }
+        var itemDiv = UIUtils.getUnselectableDiv('', cssclass);
+        return itemDiv;
+        //SIMS.Controls.Factory
+        //$iconPart.addClass('office-hover-default');
+        //return $iconPart;
+    }
+});
 namespace("ItemGenerators");
 ItemGenerators.Text = ItemGenerators.ItemBase.extend();
 namespace("ItemGenerators");
