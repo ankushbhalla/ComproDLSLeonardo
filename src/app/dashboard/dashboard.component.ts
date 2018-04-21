52ad87512f5811ed0a55e0c9d8993b7f9b38140c
Chirag Khanna <Chirag@COMPRO>
2018-04-21 12:48:20 +0530

    code changes for drop down and json changes

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,48 +9,48 @@ export class DashboardComponent implements OnInit {
   questions: Array<Object>;
   constructor() {
     this.questions = [
-      {
-        qText: "Trial Balance: Learn",
-        description: "Learn to prepare Trial Balance to verify that debits equal the credits",
-        badges:['Accounting','Training'],
-        view:"Side by Side View",
-        index:1
-      },
-      {
-        qText: "Income Statement: Learn",
-        description: "Learn to prepare Income Statement to access the profitability of a company during a particular time interval",
-        badges:['Accounting','Training'],
-        view:"Side by Side View",
-        index:2
-      },
-      {
-        qText: "Create a Motion Profile",        
-        description: "Motion profile is simply a way of defining how an Axis moves with respect to time.",
-        badges:['Physics','Training'],
-        view:"Reading View",
-        index:3
-      },
-      {
-        qText: "Trial Balance: Assess",
-        description: "Assess student's knowledge and skill at the end of the course",
-        badges:['Accounting','Assessment'],
-        view:"Side by Side View",
-        index:1
-      },
-      {
-        qText: "Income Statement: Assess",
-        description: "Assess student's knowledge and skill at the end of the course",
-        badges:['Accounting','Assessment'],
-        view:"Side by Side View",
-        index:2
-      },
-      {
-        qText: "Solve Projectile Problem",
-        description: "Assess student's knowledge and skill at the end of the course",
-        badges:['Physics','Assessment'],
-        view:"Side by Side View",
-        index:3
-      },
+      // {
+      //   qText: "Trial Balance: Learn",
+      //   description: "Learn to prepare Trial Balance to verify that debits equal the credits",
+      //   badges:['Accounting','Training'],
+      //   view:"Side by Side View",
+      //   index:1
+      // },
+      // {
+      //   qText: "Income Statement: Learn",
+      //   description: "Learn to prepare Income Statement to access the profitability of a company during a particular time interval",
+      //   badges:['Accounting','Training'],
+      //   view:"Side by Side View",
+      //   index:2
+      // },
+      // {
+      //   qText: "Create a Motion Profile",        
+      //   description: "Motion profile is simply a way of defining how an Axis moves with respect to time.",
+      //   badges:['Physics','Training'],
+      //   view:"Reading View",
+      //   index:3
+      // },
+      // {
+      //   qText: "Trial Balance: Assess",
+      //   description: "Assess student's knowledge and skill at the end of the course",
+      //   badges:['Accounting','Assessment'],
+      //   view:"Side by Side View",
+      //   index:1
+      // },
+      // {
+      //   qText: "Income Statement: Assess",
+      //   description: "Assess student's knowledge and skill at the end of the course",
+      //   badges:['Accounting','Assessment'],
+      //   view:"Side by Side View",
+      //   index:2
+      // },
+      // {
+      //   qText: "Solve Projectile Problem",
+      //   description: "Assess student's knowledge and skill at the end of the course",
+      //   badges:['Physics','Assessment'],
+      //   view:"Side by Side View",
+      //   index:3
+      // },
       {
         qText: "Exhaust System inspection",
         description: "Assess student's knowledge and skill at the end of the course",
